812457a24bee0df6ede4cbbb23bffd44e28cc79f
Shavius <glamurparty@ukr.net>
2023-03-27 22:59:22 +0300

    Add coments from function division

--- src/ts/modules/division.ts
+++ src/ts/modules/division.ts
@@ -1,3 +1,9 @@
+/**
+ * Function division
+ * @param num1 Number 1
+ * @param num2 Number 2
+ * @returns String
+ */
 export default function division(num1 = 0, num2 = 0): string {
 	const res = num1 / num2;
 
