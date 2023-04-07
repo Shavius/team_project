ef4c20e820cde634632649c4c3f5d03f2479b815
Kangern <kangern@ukr.net>
2023-04-07 20:17:18 +0300

    Update change documentation from functions
    change documentation division
    change documentation multiplication

--- src/ts/modules/multiplication.ts
+++ src/ts/modules/multiplication.ts
@@ -1,8 +1,9 @@
 /**
- * Function multiplication
- * @param num1 Number 1
- * @param num2 Number 2
- * @returns String
+ * Функция умножает два числа и возвращает строку с ответом.
+ *
+ * @param num1 - Первое число для умножения. Если аргумент не передан, то по умолчанию равен 0.
+ * @param num2 - Второе число для умножения. Если аргумент не передан, то по умолчанию равен 0.
+ * @returns Строка с ответом в формате "answer {res}", где res - результат умножения num1 на num2.
  */
 export default function multiplication(num1 = 0, num2 = 0): string {
 	const res = num1 * num2;
