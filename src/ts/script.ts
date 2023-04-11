2f6deef9e05f0415eb6ba4c4708d41dff64e9eb8
Shavius <glamurparty@ukr.net>
2023-04-12 02:52:10 +0300

    added two classes DynamicElement and CreateElementClick

--- src/ts/script.ts
+++ src/ts/script.ts
@@ -1,2 +1,21 @@
 import "../index.html";
 import "../scss/style.scss";
+import DynamicElement from "./modules/DynamicElement";
+import CreateElementClick from "./modules/CreateElementClick";
+
+const elementButton: HTMLElement | null = document.querySelector("#element1button1");
+const elementContent: HTMLElement | null = document.querySelector("#element1content1");
+let elementNumber: number = 0;
+
+if (elementButton != null && elementContent != null) {
+	elementButton.onclick = function () {
+		elementNumber += 1;
+		const elem = new DynamicElement("element__content-item");
+		elem.addElementContent(`Привет ${elementNumber}`);
+		elementContent.append(elem.getElement());
+	};
+}
+
+const elem = new CreateElementClick("element__content-item", "#element2button1", "#element2content1", "Привет");
+
+elem.initialClick();
