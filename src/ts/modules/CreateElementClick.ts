393792ba2ffb269e7a0a0f3e5f013eff0ba2d6ee
Shavius <glamurparty@ukr.net>
2023-04-12 03:06:18 +0300

    Add documentation in CreateElementClick

--- src/ts/modules/CreateElementClick.ts
+++ src/ts/modules/CreateElementClick.ts
@@ -1,18 +1,47 @@
 import DynamicElement from "./DynamicElement";
 
+/**
+ * Класс `CreateElementClick` представляет собой расширение класса `DynamicElement` и предоставляет
+ * функциональность для создания и добавления динамических HTML-элементов при клике на кнопку.
+ */
 export default class CreateElementClick extends DynamicElement {
+	/**
+	 * Имя кнопки HTML-элемента, на которую будет добавлен обработчик клика.
+	 */
 	elementButtonName: string | undefined;
 
+	/**
+	 * Имя HTML-элемента, в который будут добавлены созданные элементы при клике на кнопку.
+	 */
 	elementContentName: string | undefined;
 
+	/**
+	 * Кнопка HTML-элемента, на которую будет добавлен обработчик клика.
+	 */
 	elementButton: HTMLElement | null;
 
+	/**
+	 * HTML-элемент, в который будут добавлены созданные элементы при клике на кнопку.
+	 */
 	elementContent: HTMLElement | null;
 
+	/**
+	 * Содержимое, которое будет добавлено в создаваемые HTML-элементы.
+	 */
 	content: string;
 
+	/**
+	 * Счетчик количества созданных элементов.
+	 */
 	elementContentNumber: number = 0;
 
+	/**
+	 * Создает экземпляр класса `CreateElementClick`.
+	 * @param htmlElementTagName Имя тега HTML-элемента, который будет создан при вызове метода `createElement` унаследованного от класса `DynamicElement`.
+	 * @param elementButtonName Имя кнопки HTML-элемента, на которую будет добавлен обработчик клика.
+	 * @param elementContentName Имя HTML-элемента, в который будут добавлены созданные элементы при клике на кнопку.
+	 * @param content Содержимое, которое будет добавлено в создаваемые HTML-элементы.
+	 */
 	constructor(htmlElementTagName: string, elementButtonName: string, elementContentName: string, content: string) {
 		super(htmlElementTagName);
 		this.elementButton = document.querySelector(elementButtonName);
@@ -20,6 +49,9 @@ export default class CreateElementClick extends DynamicElement {
 		this.content = content;
 	}
 
+	/**
+	 * Метод `initialClick` добавляет обработчик клика на кнопку HTML-элемента, который создает новый HTML-элемент с содержимым и добавляет его в указанный элемент при клике на кнопку.
+	 */
 	initialClick(): void {
 		if (this.elementButton != null && this.elementContent != null) {
 			this.elementButton.onclick = () => {
