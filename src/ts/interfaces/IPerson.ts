97dbc2a8c3cafc38bd8950a90a2bc9305d4e7c1d
Kangern <kangern@ukr.net>
2023-08-14 23:18:14 +0300

    Add class Person

---
+++ src/ts/interfaces/IPerson.ts
@@ -0,0 +1,5 @@
+export default interface IPerson {
+	name: string;
+	age: number;
+	race: string;
+}
---
+++ src/ts/modules/Person.ts
@@ -0,0 +1,19 @@
+import IPerson from "../interfaces/IPerson";
+
+export default class Person implements IPerson {
+	name: string;
+
+	age: number;
+
+	race: string;
+
+	constructor(name: string, age: number, race: string) {
+		this.name = name;
+		this.age = age;
+		this.race = race;
+	}
+
+	getInfoPerson(): string {
+		return `Имя: ${this.name} Возраст: ${this.age} Раса: ${this.race}`;
+	}
+}
