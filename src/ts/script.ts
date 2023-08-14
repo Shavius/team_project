ffabea29aeef48823a68ab3eebbec01cedfd9cac
Kangern <kangern@ukr.net>
2023-08-14 23:20:55 +0300

    Add in script.ts class Person and CharacterDisplay

--- src/ts/script.ts
+++ src/ts/script.ts
@@ -2,6 +2,8 @@ import "../index.html";
 import "../scss/style.scss";
 import DynamicElement from "./modules/DynamicElement";
 import CreateElementClick from "./modules/CreateElementClick";
+import Person from "./modules/Person";
+import CharacterDisplay from "./modules/CharacterDisplay";
 
 const elementButton: HTMLElement | null = document.querySelector("#element1button1");
 const elementContent: HTMLElement | null = document.querySelector("#element1content1");
@@ -19,3 +21,15 @@ if (elementButton != null && elementContent != null) {
 const elem = new CreateElementClick("element__content-item", "#element2button1", "#element2content1", "Привет");
 
 elem.initialClick();
+
+const characterPersonList: string[] = [
+	new Person("Alan", 31, "Human").getInfoPerson(),
+	new Person("Minara", 28, "Human").getInfoPerson(),
+	new Person("Eyla", 120, "Elf").getInfoPerson(),
+	new Person("Neyra", 130, "Elf").getInfoPerson(),
+];
+
+const personDisply: HTMLElement | null = document.querySelector(".person__element");
+const personBtn: HTMLElement | null = document.querySelector(".person__btn");
+
+new CharacterDisplay(characterPersonList, personDisply, personBtn).showCharacterFromList();
