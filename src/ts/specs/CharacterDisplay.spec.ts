8ab6d8e85df7203a795a4c051658580689e189fa
Tolgran <tolgran@ukr.net>
2023-08-24 13:26:15 +0300

    Убрать комментарии в тесте CharacterDisplay.spec.ts #26

--- src/ts/specs/CharacterDisplay.spec.ts
+++ src/ts/specs/CharacterDisplay.spec.ts
@@ -26,24 +26,21 @@ describe("CharacterDisplay class tests", () => {
 	it("should display characters on button click", () => {
 		characterDisplay.showCharacterFromList();
 
-		btnMock.click(); // Simulate a button click
+		btnMock.click();
 
-		expect(displayMock.childElementCount).toBe(1); // Check if a character div is added to the display
-		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-		expect(displayMock.firstChild!.textContent).toBe("Character 1"); // Check if the displayed text is correct
+		expect(displayMock.childElementCount).toBe(1);
+		expect(displayMock.firstChild!.textContent).toBe("Character 1");
 	});
 
 	it("should display multiple characters sequentially on button clicks", () => {
 		characterDisplay.showCharacterFromList();
 
-		btnMock.click(); // First click
-		btnMock.click(); // Second click
+		btnMock.click();
+		btnMock.click();
 
-		expect(displayMock.childElementCount).toBe(2); // Check if two character divs are added to the display
-		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-		expect(displayMock.firstChild!.textContent!).toBe("Character 1"); // Check if the displayed text from the first click is correct
-		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-		expect(displayMock.lastChild!.textContent!).toBe("Character 2"); // Check if the displayed text from the second click is correct
+		expect(displayMock.childElementCount).toBe(2);
+		expect(displayMock.firstChild!.textContent!).toBe("Character 1");
+		expect(displayMock.lastChild!.textContent!).toBe("Character 2");
 	});
 
 	it("should not display characters if display or button is null", () => {
@@ -54,6 +51,6 @@ describe("CharacterDisplay class tests", () => {
 
 		btnMock.click();
 
-		expect(displayMock.childElementCount).toBe(0); // Check that no character div is added to the display
+		expect(displayMock.childElementCount).toBe(0);
 	});
 });
