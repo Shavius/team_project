9ea8d9bf2a8492291ba611e819ef56451bc687c4
Legrendorg <Legrendorg@ukr.net>
2023-08-25 20:01:41 +0300

    Добавить две функции сложение и вычитание #33
    Добавить файл utils.ts #32

---
+++ src/ts/modules/utils.ts
@@ -0,0 +1,7 @@
+export function textAndAddition(text: string, num1: number, num2: number): string {
+	return `${text} ${num1 + num2}`;
+}
+
+export function textAndSubtraction(text: string, num1: number, num2: number): string {
+	return `${text} ${num1 - num2}`;
+}
